4c7864506e28ebedd00445594e713b8c3016ea0d
Brian Bustin <brian@bustin.us>
2019-05-22 13:53:11 -0400

    Initial release. It can parse DMARC XML files and gzipped DMARC XML files. Currently can not parse ZIPed files.

---
+++ src/aggregate_report.rs
@@ -0,0 +1,165 @@
+// Based upon appendix C of the DMARC RFC
+// https://tools.ietf.org/html/rfc7489#appendix-C
+
+// Tested against a large number of reports I have from various places and
+// had to make certain fields optional even though the spec says they are not.
+// Also had to make the version field a String.
+// Guess the spec is not being followed to a T.
+use std::net::Ipv4Addr;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DateRangeType {
+    pub begin:  u32,
+    pub end:    u32
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ReportMetadataType {
+    pub org_name:           String,
+    pub email:              String,
+    pub extra_contact_info: Option<String>,
+    pub report_id:          String,
+    pub date_range:         DateRangeType,
+    pub error:              Option<Vec<String>>
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum AlignmentType {
+    r,
+    s
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DispositionType {
+    none,
+    quarantine,
+    reject
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PolicyPublishedType {
+    pub domain:     String,
+    pub adkim:      Option<AlignmentType>,
+    pub aspf:       Option<AlignmentType>,
+    pub p:          DispositionType,
+    pub sp:         Option<DispositionType>,
+    pub pct:        u8,
+    pub fo:         Option<String>
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DMARCResultType {
+    pass,
+    fail
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum PolicyOverrideType {
+    forwarded,
+    sampled_out,
+    trusted_forwarder,
+    mailing_list,
+    local_policy,
+    other
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PolicyOverrideReason {
+    pub r#type:     PolicyOverrideType,
+    pub comment:    Option<String>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PolicyEvaluatedType {
+    pub disposition:    DispositionType,
+    pub dkim:           Option<DMARCResultType>,
+    pub spf:            Option<DMARCResultType>,
+    pub reason:         Option<Vec<PolicyOverrideReason>>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RowType {
+    pub source_ip:          Ipv4Addr,
+    pub count:              u32,
+    pub policy_evaluated:   PolicyEvaluatedType
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct IdentifierType {
+    pub envelope_to:    Option<String>,
+    pub envelope_from:  Option<String>,
+    pub header_from:    String
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DKIMResultType {
+    none,
+    pass,
+    fail,
+    policy,
+    neutral,
+    temperror,
+    permerror
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DKIMAuthResultType {
+    pub domain:         String,
+    pub selector:       Option<String>,
+    pub result:         DKIMResultType,
+    pub human_result:   Option<String>
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum SPFDomainScope {
+    helo,
+    mfrom
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub enum SPFResultType {
+    none,
+    neutral,
+    pass,
+    fail,
+    softfail,
+    temperror,
+    permerror
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SPFAuthResultType {
+    pub domain: String,
+    pub scope:  Option<SPFDomainScope>,
+    pub result: SPFResultType
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthResultType {
+    pub dkim:   Option<Vec<DKIMAuthResultType>>,
+    pub spf:    Vec<SPFAuthResultType>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RecordType {
+    pub row:            RowType,
+    pub identifiers:    IdentifierType,
+    pub auth_results:   AuthResultType
+}
+
+#[allow(non_camel_case_types)]
+#[derive(Debug, Serialize, Deserialize)]
+pub struct feedback {
+    pub version:            Option<String>,
+    pub report_metadata:    ReportMetadataType,
+    pub policy_published:   PolicyPublishedType,
+    pub record:             Vec<RecordType>
+}
---
+++ src/lib.rs
@@ -0,0 +1,88 @@
+#![deny(missing_debug_implementations)]
+pub mod aggregate_report;
+
+use std::fs::{File, read_dir};
+use std::io::BufReader;
+use std::path::Path;
+use serde_xml_rs::from_reader;
+use libflate::gzip;
+use std::io::Read;
+
+pub fn parse(path: &Path) -> Result<aggregate_report::feedback, Box<std::error::Error>> {
+    if path.is_dir() {
+        panic!("{} is a directory")
+    }
+
+    let extension = path.extension();
+
+    match extension {
+        Some(extension) => {
+            match extension.to_str().unwrap() {
+                "xml" => {
+                    let mut reader = get_file_reader(&path)?;
+                    return parse_reader(&mut reader);
+                },
+                "gz" => {
+                    let reader = get_file_reader(&path)?;
+                    let mut decoder = gzip::Decoder::new(reader)?;
+                    return parse_reader(&mut decoder);
+                },
+                _       => {
+                    let error_message = format!("Do not know how to handle {} files :-(", extension.to_str().unwrap());
+                    return Err(error_message.into());
+                }
+            }
+        },
+        None            => ()
+    }
+
+    let file = File::open(&path)?;
+    let mut file = BufReader::new(file);
+
+    parse_reader(&mut file)
+}
+
+fn get_file_reader(path: &Path) -> Result<BufReader<File>, Box<std::error::Error>> {
+    let file = File::open(&path)?;
+    Ok(BufReader::new(file))
+}
+
+fn parse_reader(reader: &mut Read) -> Result<aggregate_report::feedback, Box<std::error::Error>> {
+    match from_reader(reader) {
+        Ok(result) => Ok(result),
+        Err(error) => Err(error.into())
+    }
+}
+
+pub fn parse_dir(path: &Path) -> Vec<aggregate_report::feedback> {
+    let mut results = Vec::new();
+
+    if path.is_dir() {
+        for entry in read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+
+            let result = parse(&path);
+            match result {
+                Ok(result) => results.push(result),
+                Err(error) => println!("could not parse: {:?} because {:?}", path, error)
+            }
+        }
+    }
+    
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse_single() {
+        parse("dmarc.xml");
+    }
+
+    #[test]
+    fn test_parse_dir() {
+        parse_dir(Path::new("./"));
+    }
+}
