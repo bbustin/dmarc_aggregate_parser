6d349ff85a204c5cb0ba4a54c8cc0fddc10c5184
Vladimir 'join3r' Schmotzer <vladimir.schmotzer@dxc.com>
2022-10-13 09:23:21 +0200

    generic parameter for parse

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,8 @@ use std::io::BufReader;
 use std::io::Read;
 use std::path::Path;
 
-pub fn parse(path: &Path) -> Result<aggregate_report::feedback, ParsingError> {
+pub fn parse<T: std::convert::AsRef<std::ffi::OsStr>>(path: T) -> Result<aggregate_report::feedback, ParsingError> {
+    let path = std::path::Path::new(&path);
     if path.is_dir() {
         panic!("path is a directory")
     }
